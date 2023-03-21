21862413f179c730705e0ddfa1551ceb13c55d92
Mayank <mayankgoyal.btprod20@pec.edu.in>
2023-03-22 00:06:23 +0530

    checkbox prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import ButtonM from "./components/ButtonM";
+import CheckBoxM from "./components/CheckBoxM";
 
 function App() {
   return (
     <div className="App">
       <h1>Material Ui</h1>
       <ButtonM />
+      <CheckBoxM />
     </div>
   );
 }
---
+++ src/components/CheckBoxM.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Checkbox } from "@mui/material";
+import { Favorite } from "@mui/icons-material";
+import { FavoriteBorder } from "@mui/icons-material";
+
+const CheckBoxM = () => {
+  const [name, setName] = useState([]);
+
+  const getValue = (e) => {
+    let data = name;
+    data.push(e.target.value);
+    console.warn(data);
+  };
+
+  return (
+    <div>
+      <h1>React Material UI | CheckBox</h1>
+      <Checkbox color="primary" value="anil" onChange={(e) => getValue(e)} />
+      <Checkbox color="primary" value="sam" onChange={(e) => getValue(e)} />
+      <Checkbox color="primary" value="peter" onChange={(e) => getValue(e)} />
+      <Checkbox
+        color="primary"
+        value="xyz"
+        indeterminate
+        onChange={(e) => getValue(e)}
+      />
+      <Checkbox
+        value="abc"
+        color="secondary"
+        onChange={(e) => getValue(e)}
+        icon={<FavoriteBorder />}
+        checkedIcon={<Favorite />}
+      />
+    </div>
+  );
+};
+
+export default CheckBoxM;
